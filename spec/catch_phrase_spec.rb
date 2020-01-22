172b795615340fdc08bef5321fafda726dfb98f5
Jessie Tivey <jesstivey@gmail.com>
2020-01-22 20:40:54 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -4,6 +4,5 @@ describe "#catch_phrase" do
   it "puts out a catch phrase" do 
     phrase = "It's-a me, Mario!"
     expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
-    puts phrase
   end
 end
